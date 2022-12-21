47f6074f0b873a61a156546f924ca8c5b7e4d109
Hotiakov <fauxlivegame@gmail.com>
2022-12-21 15:23:39 +0300

    update

--- server/routes/publication-api.js
+++ server/routes/publication-api.js
@@ -1,10 +1,8 @@
 module.exports = function(app, db) {
     console.log(db);
     app.post('/api/v1/publication', (req, res) => {
-        console.log(db);
         db.collection('publications').find({}).toArray(function(err, results){
             if(err) return console.log(err);
-            console.log(results);
             res.send(results);
         });
         // const {data, page} = req.body;
