e31c2dd8485d9ffd2a53ae19fc06b9f403809a68
Анастасия Лихашва <kassiopea308@mail.ru>
2022-12-25 16:36:52 +0300

    add description page

--- client/src/pages/DescriptionPage/index.jsx
+++ client/src/pages/DescriptionPage/index.jsx
@@ -1,9 +1,13 @@
+import BackButton from 'components/BackButton/BackButton';
 import React from 'react';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import {useLocation} from "react-router-dom";
 import './style.scss';
 
 const DescriptionPage = () => {
 
-    const [users, setUsers] = React.useState([]);
+    /*const [users, setUsers] = React.useState([]);
 
     const getUsers = async () => {
         let data = await fetch('http://localhost:8000/api/v1/hello');
@@ -13,11 +17,40 @@ const DescriptionPage = () => {
             data = await data.json();
             setUsers(data);
         }
-    }
+    }*/
 
     return(
-        <div className='main'>
-            
+        <div className='description'>
+            <div class="description__container">
+                <div class="description__block">
+                    <div class="description__block-desc">
+                        <BackButton link='../'/>
+                        <h1 class="description__block-title">
+                            Описание публикации
+                        </h1>
+                    </div>
+                    <div class="description-block-info">
+                        <h2 class="description-block-info__title">
+                            Название публикации
+                        </h2>
+                        <div class="description-block-info__author">
+                            Автор публикации
+                        </div>
+                        <div class="description-block-info__edition">
+                            <div class="description-block-info__type-edition">Тип издания: </div>
+                            <div class="description-block-info__name-edition">Название издания</div>
+                        </div>
+                        <div class="description-block-info__year-and-index">
+                            <div class="description-block-info__year">Год издания</div>
+                            <div class="description-block-info__index">Индекс издания</div>
+                        </div>
+                        <div class="description-block-info__annotation">
+                            <div class="description-block-info__annotation-title">Аннотация:</div>
+                            <div class="description-block-info_annotation-text"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
